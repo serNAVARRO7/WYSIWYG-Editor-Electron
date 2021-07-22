5aa0e32e8f661c3038211d83ee1e5102da213fba
Sergio Navaro <sergio@orvium.io>
2021-07-22 09:04:05 +0200

    Added WYSIWYG editor library

--- my-editor/src/app/app.module.ts
+++ my-editor/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { NgxWigModule } from 'ngx-wig';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxWigModule
   ],
   providers: [],
   bootstrap: [AppComponent]
